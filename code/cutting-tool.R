1ff362df2fb643cc30ab3f1ee19bbbc6c69af680
bgreenwell <greenwell.brandon@gmail.com>
2018-11-12 09:20:11 -0500

    update content

--- code/cutting-tool.R
+++ code/cutting-tool.R
@@ -2,6 +2,7 @@
 library(broom)
 library(dplyr)
 library(ggplot2)
+library(gridExtra)
 library(lattice)
 
 # Load the data
@@ -24,25 +25,41 @@ splom(cutting_tool, groups = cutting_tool$ToolType)
 xyplot(Hour ~ rpm, groups = ToolType, data = cutting_tool, type = c("p", "r"),
        pch = 19)
 
+# Same as above, but using ggplot2
+ggplot(cutting_tool, aes(x = rpm, y = Hour, group = ToolType)) +
+  geom_point(aes(colour = ToolType), size = 3) +
+  geom_smooth(method = "lm", formula = y ~ x, se = FALSE,
+              color = "black") +
+  theme_light()
+
 # Regression model
-fit1 <- lm(Hour ~ rpm + ToolType, data = cutting_tool)
+fit <- lm(Hour ~ rpm + ToolType, data = cutting_tool)
 
 # Print model summary
-summary(fit1)
+summary(fit)
 
 # Refit the model by creating your own indicator for ToolType
 cutting_tool$ToolTypeB <- ifelse(cutting_tool$ToolType == "B", yes = 1, no = 0)
 fit2 <- lm(Hour ~ rpm + ToolTypeB, data = cutting_tool)
 
 # Residual plots
-cutting_tool_2 <- augment(fit1)
+cutting_tool_2 <- augment(fit)  # from broom package
 
 # Residual plots
-p1 <- ggplot(cutting_tool_2, aes(x = .fitted, y = .std.resid )) +
+p1 <- ggplot(cutting_tool_2, aes(x = rpm, y = .fitted)) +
+  geom_point(aes(y = Hour, colour = ToolType), size = 3) +
+  geom_line(aes(group = ToolType), color = "black") +
+  labs(x = "rpm", y = "Hour") +
+  theme_light()
+p2 <- ggplot(cutting_tool_2, aes(x = .fitted, y = .std.resid )) +
   geom_point(aes(colour = ToolType)) +
-  # geom_smooth(se = FALSE) +
-  geom_hline(yintercept = 0, linetype = "dashed")
-p2 <- ggplot(cutting_tool_2, aes(sample = .std.resid )) +
+  geom_smooth(se = FALSE) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(x = "Fitted value", y = "Studentized residual") +
+  theme_light()
+p3 <- ggplot(cutting_tool_2, aes(sample = .std.resid )) +
   geom_qq(aes(color = ToolType)) +
-  geom_qq_line()
-gridExtra::grid.arrange(p1, p2, nrow = 1)
+  geom_qq_line() +
+  labs(x = "Theoretical", y = "Sample") +
+  theme_light()
+grid.arrange(p1, p2, p3, nrow = 1)
